12bd412a136506cdf8b204e8d60a0ab7ce3b1e0c
Valent <ignacio1994.ir@gmail.com>
2024-01-09 20:05:14 -0400

    partytown added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://valentramos.com',
+  integrations: [tailwind(), partytown()],
+  site: 'https://valentramos.com'
   // base: 'valent-astro',
 });
