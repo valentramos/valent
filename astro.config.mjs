9c91b513f8f7a61175b3e48d622b921db6b241e4
valentramos <ignacio1994.ir@gmail.com>
2024-01-23 20:15:15 -0400

    auth supabase

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://valentramos.com',
+  output: 'server',
   // base: 'valent-astro',
 });
