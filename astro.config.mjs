641b7bb43b43c3ddbe6f468c9ce5bd2e319b4fbe
valentramos <ignacio1994.ir@gmail.com>
2024-02-08 12:49:05 -0400

    tag fix and flowbite script map added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()],
+  integrations: [tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })],
   site: 'https://valentramos.com',
   // output: 'server',
   // output: 'static',
